35277b14fb9224ad5e0e3de5285ffe990fbb329b
YUNJAEGOONER <coygyj@gmail.com>
2026-08-05 21:56:53 +0900

    init project

---
+++ src/test/java/com/yj/gitbranch1/GitBranch1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.gitbranch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitBranch1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
